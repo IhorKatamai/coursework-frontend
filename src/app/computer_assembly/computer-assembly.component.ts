c545a4ca1bf7d471e85ff3456030b8951705000a
Ihor <ihor.katamai@nerdysoft.com>
2023-12-12 20:51:42 +0200

    add computer assembly component logic

--- src/app/computer_assembly/computer-assembly.component.ts
+++ src/app/computer_assembly/computer-assembly.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-computer-assembly',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./computer-assembly.component.scss']
 })
 export class ComputerAssemblyComponent {
+  form: FormGroup;
+  loading = false;
 
+  constructor(private router: Router, private http: HttpClient) {
+    this.form = new FormGroup({
+      computerModel: new FormControl('', [Validators.required]),
+      delivery: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [Validators.required]),
+      deliveryAddress: new FormControl('', [Validators.required]),
+      urgent: new FormControl('0', [Validators.required]),
+      payment: new FormControl('card', [Validators.required]),
+      additionalInfo: new FormControl('')
+    });
+  }
+
+  submit() {
+    this.loading = true;
+    this.http.post(
+      'http://localhost:8080/api/computer-assembly',
+      { ...this.form.value }
+    ).subscribe(() => {
+      this.loading = false;
+      this.returnBack();
+    });
+  }
+
+  returnBack() {
+    this.router.navigate(['/']);
+  }
 }
